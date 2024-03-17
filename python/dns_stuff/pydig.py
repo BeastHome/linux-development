f22b1d5161c7ffd20887e0e8ae982ca1a01f2556
David Harris <dave@harris-services.com>
2024-03-17 15:13:55 -0500

    Added static resolver for Level3 to prevent DNSSEC issues, and implemented TXT record display workaround.

--- python/dns_stuff/pydig.py
+++ python/dns_stuff/pydig.py
@@ -18,18 +18,22 @@ try:
 except IndexError:
     domain_name = input ("Enter the domain to check: ")
 
+# Sets the resolver to one that works with DNSSEC.
+resolver = dns.resolver.Resolver()
+resolver.nameservers = ['4.2.2.1']
+
 # Print the header.
 print (f"DNS for {domain_name}.\n")
 
 # Print the A record(s) for the domain.
-a_answers = dns.resolver.resolve(domain_name, 'A')
+a_answers = resolver.resolve(domain_name, 'A')
 print ("A record(s):")
 for rdata in a_answers:
     print(rdata.address)
 print()
 
 # Print the SOA record for the domain.
-soa_answers = dns.resolver.resolve(domain_name, 'SOA')
+soa_answers = resolver.resolve(domain_name, 'SOA')
 print ("SOA:")
 for rdata in soa_answers:
     print(f' serial: {rdata.serial} | tech: {rdata.rname} | mname: {rdata.mname}')
@@ -37,29 +41,31 @@ for rdata in soa_answers:
 print()
 
 # Print the nameservers for the domain with the related A record(s).
-ns_answers = dns.resolver.resolve(domain_name, 'NS')
+ns_answers = resolver.resolve(domain_name, 'NS')
 print("Nameservers:")
 for server in ns_answers:
     print(server.target)
-    a_answers = dns.resolver.resolve(server.target, 'A')
+    a_answers = resolver.resolve(server.target, 'A')
     for rdata2 in a_answers:
         print(rdata2.address)
     print()
 
 # Print the MX records for the domain with the related A record(s).
-mx_answers = dns.resolver.resolve(domain_name, 'MX',)
+mx_answers = resolver.resolve(domain_name, 'MX',)
 print ("MX:")
 for rdata in mx_answers:
     print('Host', rdata.exchange, 'has preference', rdata.preference)
-    a_answers = dns.resolver.resolve(rdata.exchange, 'A')
+    a_answers = resolver.resolve(rdata.exchange, 'A')
     for rdata3 in a_answers:
         reverse_addr = dns.reversename.from_address(rdata3.address)
-        print(rdata3.address, 'PTR:', dns.resolver.resolve(reverse_addr, "PTR")[0])
+        print(rdata3.address, 'PTR:', resolver.resolve(reverse_addr, "PTR")[0])
     print()
 
 # Print the TXT record(s) for the domain.
-txt_answers = dns.resolver.resolve(domain_name, 'TXT')
+txt_answers = resolver.resolve(domain_name, 'TXT')
 print ("TXT:")
 for rdata in txt_answers:
     for txt_string in rdata.strings:
-        print (' TXT:', txt_string)
+        txt_value = str(txt_string)
+        trimmed_txt_value = txt_value[1:]
+        print (trimmed_txt_value)
